6d17ded6e5f80396b97468b2534796947ee0ba50
Teresa Loafman <teresasanders77@gmail.com>
2021-03-25 11:36:49 -0600

    67. Generate HTML Based on JSON

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -44,8 +44,14 @@ class Search {
     }
 
     getResults() {
-        $.getJSON('http://fictional-university/wp-json/wp/v2/posts?search=biology', function(posts){
-            alert(posts[0].title.rendered);
+        $.getJSON('http://fictional-university/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+            var testArray = ['red', 'orange', 'yellow'];    
+            this.resultsDiv.html(`
+                <h2 class ="search-overlay__section-title">General Information</h2>
+                <ul class="link-list min-list">
+                    ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+                </ul>
+                `);
         });
     }
 
