015769879f1071a1ca5ec029fb6a9e93de185340
Teresa Loafman <teresasanders77@gmail.com>
2021-03-30 10:35:10 -0600

    90. My Notes front-end pt.1

---
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -0,0 +1,18 @@
+import $ from "jQuery";
+
+class MyNotes {
+    constructor() {
+        this.events();
+    }
+
+    events() {
+        $(".delete-note").on("click", this.deleteNote);
+    }
+
+    //Methods will go here
+    deleteNote() {
+        alert("you clicked delete");
+    }
+}
+
+export default MyNotes;
--- wp-content/themes/fictional-university-theme/js/scripts.js
+++ wp-content/themes/fictional-university-theme/js/scripts.js
@@ -1,20 +1,20 @@
-import "../css/style.css"
+import "../css/style.css";
 
 // Our modules / classes
-import MobileMenu from "./modules/MobileMenu"
-import HeroSlider from "./modules/HeroSlider"
-import GoogleMap from "./modules/GoogleMap"
-import Search from "./modules/Search"
-
+import MobileMenu from "./modules/MobileMenu";
+import HeroSlider from "./modules/HeroSlider";
+import GoogleMap from "./modules/GoogleMap";
+import Search from "./modules/Search";
+import MyNotes from "./modules/MyNotes";
 
 // Instantiate a new object using our modules/classes
-var mobileMenu = new MobileMenu()
-var heroSlider = new HeroSlider()
-const googleMap = new GoogleMap()
-const search = new Search()
+var mobileMenu = new MobileMenu();
+var heroSlider = new HeroSlider();
+const googleMap = new GoogleMap();
+const search = new Search();
+const mynotes = new MyNotes();
 
 // Allow new JS and CSS to load in browser without a traditional page refresh
 if (module.hot) {
-  module.hot.accept()
+    module.hot.accept();
 }
-
