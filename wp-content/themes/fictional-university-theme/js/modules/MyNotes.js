4e815fa10474056818ff466f35fc47af9f797828
Teresa Loafman <teresasanders77@gmail.com>
2021-03-30 14:22:06 -0600

    94. Edit /update posts fix

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -1,4 +1,4 @@
-import $ from "jQuery";
+import $ from "jquery";
 
 class MyNotes {
     constructor() {
@@ -22,24 +22,25 @@ class MyNotes {
     }
 
     makeNoteEditable(thisNote) {
+        thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden="true"></i> Cancel');
+        thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+        thisNote.find(".update-note").addClass("update-note--visible");
+        thisNote.data("state", "editable");
+    }
+
+    makeNoteReadOnly(thisNote) {
         thisNote.find(".edit-note").html('<i class="fa fa-pencil" aria-hidden="true"></i> Edit');
         thisNote
             .find(".note-title-field, .note-body-field")
             .attr("readonly", "readonly")
             .removeClass("note-active-field");
         thisNote.find(".update-note").removeClass("update-note--visible");
-        thisNote.data("state", "editable");
-    }
-
-    makeNoteReadOnly(thisNote) {
-        thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden="true"></i> Cancel');
-        thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
-        thisNote.find(".update-note").addClass("update-note--visible");
         thisNote.data("state", "cancel");
     }
 
     deleteNote(e) {
         var thisNote = $(e.target).parents("li");
+
         $.ajax({
             beforeSend: (xhr) => {
                 xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
@@ -60,10 +61,12 @@ class MyNotes {
 
     updateNote(e) {
         var thisNote = $(e.target).parents("li");
+
         var ourUpdatedPost = {
             title: thisNote.find(".note-title-field").val(),
             content: thisNote.find(".note-body-field").val()
         };
+
         $.ajax({
             beforeSend: (xhr) => {
                 xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
