cfe61c17a6a5590cdb4789378e411cd8f0362a35
Teresa Loafman <teresasanders77@gmail.com>
2021-03-30 10:54:51 -0600

    91. My Notes Front-end pt. 2

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -9,9 +9,23 @@ class MyNotes {
         $(".delete-note").on("click", this.deleteNote);
     }
 
-    //Methods will go here
+    // Methods will go here
     deleteNote() {
-        alert("you clicked delete");
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+            },
+            url: universityData.root_url + "/wp-json/wp/v2/note/87",
+            type: "DELETE",
+            success: (response) => {
+                console.log("Congrats");
+                console.log(response);
+            },
+            error: (response) => {
+                console.log("Sorry");
+                console.log(response);
+            }
+        });
     }
 }
 
