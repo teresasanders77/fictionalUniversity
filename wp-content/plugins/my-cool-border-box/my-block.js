320b1df4eb54041d7906da2c0a3e2a6650a25447
Teresa Loafman <teresasanders77@gmail.com>
2021-04-07 10:12:59 -0600

    119. Create a Custom Block type Pt. 2

--- wp-content/plugins/my-cool-border-box/my-block.js
+++ wp-content/plugins/my-cool-border-box/my-block.js
@@ -7,16 +7,37 @@ wp.blocks.registerBlockType("teresa/border-box", {
         color: { type: "string" }
     },
     edit: function (props) {
+        function updateContent(event) {
+            props.setAttributes({ content: event.target.value });
+        }
+
+        function updateColor(value) {
+            props.setAttributes({ color: value.hex });
+        }
         return React.createElement(
             "div",
             null,
-            wp.element.createElement("h3", null, "Your Cool Border Box"),
-            React.createElement("input", {
-                type: "text"
+            /*#__PURE__*/ React.createElement("h3", null, "Your Cool Border Box"),
+            /*#__PURE__*/ React.createElement("input", {
+                type: "text",
+                value: props.attributes.content,
+                onChange: updateContent
+            }),
+            /*#__PURE__*/ React.createElement(wp.components.ColorPicker, {
+                color: props.attributes.color,
+                onChangeComplete: updateColor
             })
         );
     },
     save: function (props) {
-        return null;
+        return React.createElement(
+            "h3",
+            {
+                style: {
+                    border: `5px solid ${props.attributes.color}`
+                }
+            },
+            props.attributes.content
+        );
     }
 });
